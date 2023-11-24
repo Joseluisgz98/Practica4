8e44954e3cef6b04fbc2a049dcc17129e14ad94f
joseluisgz98 <joseluis_cadiz98@hotmail.com>
2023-11-24 19:29:52 +0100

    nose porq falla

--- app/src/main/java/com/example/practica4/Clase/Superhero.kt
+++ app/src/main/java/com/example/practica4/Clase/Superhero.kt
@@ -2,7 +2,7 @@ package com.example.practica4.Clase
 import androidx.annotation.DrawableRes
 
 data class Superhero( val superheroName: String,
-                      val publisher: String,
                       val realName: String,
+                      val publisher: String,
                     @DrawableRes val photo: Int)
 
--- app/src/main/java/com/example/practica4/SuperHero.kt
+++ app/src/main/java/com/example/practica4/SuperHero.kt
@@ -1,19 +1,21 @@
 package com.example.practica4
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,8 +23,7 @@ import androidx.compose.ui.unit.dp
 import com.example.practica4.Clase.Superhero
 
 
-
-fun getSuperheroes():List<Superhero>{
+fun getSuperheroes(): List<Superhero> {
     return listOf(
         Superhero("Spiderman", "Petter Parker", "Marvel", R.drawable.spiderman),
         Superhero("Wolverine", "James Howlett", "Marvel", R.drawable.logan),
@@ -33,6 +34,7 @@ fun getSuperheroes():List<Superhero>{
         Superhero("Wonder Woman", "Princess Diana", "DC", R.drawable.wonder_woman)
     )
 }
+
 @Preview(showBackground = true)
 @Composable
 fun SuperHeroView() {
@@ -44,13 +46,33 @@ fun SuperHeroView() {
 }
 
 @Composable
-fun ItemHero(superhero: Superhero){
-    Card( modifier = Modifier
-        .size(200.dp)
-        .background(Color.Red, shape = RoundedCornerShape(2.dp))) {
+fun ItemHero(superhero: Superhero) {
+    Card(
+        border = BorderStroke(2.dp, Color.Red),
+        modifier = Modifier
+            .size(200.dp)
+            .padding(4.dp)
+    ) {
         Column {
-            Image(painter = painterResource(id = superhero.photo), contentDescription = "SuperHero Avatar" , contentScale = ContentScale.Crop, modifier = Modifier.width(200.dp))
-            
+            Image(
+                painter = painterResource(id = superhero.photo),
+                contentDescription = "SuperHero Avatar",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally))
+            Text(
+                text = superhero.superheroName,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = superhero.realName,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = superhero.publisher,
+                modifier = Modifier.align(Alignment.End)
+            )
         }
 
     }
